66464c020d2c59f02ca5f578e1ce7c8e4568a6a3
Undermuz <undermuz@gmail.com>
2022-07-20 20:34:27 +0300

    fix rsuite header

--- src/themes/rsuite/components.tsx
+++ src/themes/rsuite/components.tsx
@@ -1,15 +1,17 @@
 import { FC, PropsWithChildren } from "react"
 import { Panel } from "rsuite"
+import styled from "styled-components"
 import { IJsonFormProps } from "../../types"
 import { useJsonFormUi } from "../../UiContext"
 
+const UiHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
 const JsonFormComponent: FC<PropsWithChildren<IJsonFormProps>> = (props) => {
-    const {
-        title,
-        header = null,
-        primary = true,
-        children,
-    } = props
+    const { title, header = null, primary = true, children } = props
 
     const Ui = useJsonFormUi()
 
@@ -17,10 +19,10 @@ const JsonFormComponent: FC<PropsWithChildren<IJsonFormProps>> = (props) => {
         <Ui.Container>
             <Panel
                 header={
-                    <>
+                    <UiHeader>
                         {title}
                         {header}
-                    </>
+                    </UiHeader>
                 }
                 shaded={!primary}
             >
--- tools/cleanup.js
+++ tools/cleanup.js
@@ -1,34 +1,29 @@
 /* eslint-disable */
-import { existsSync, readdirSync, lstatSync, unlinkSync, rmdirSync } from "fs"
-import { join } from "path"
+const fs = require("fs")
+const Path = require("path")
 /* eslint-enable */
 
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
 const deleteFolderRecursive = (path) => {
-    if (existsSync(path)) {
-        readdirSync(path).forEach((file) => {
-            const curPath = join(path, file)
-            if (lstatSync(curPath).isDirectory()) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach((file) => {
+            const curPath = Path.join(path, file)
+            if (fs.lstatSync(curPath).isDirectory()) {
                 deleteFolderRecursive(curPath)
             } else {
-                unlinkSync(curPath)
+                fs.unlinkSync(curPath)
             }
         })
-        rmdirSync(path)
+        fs.rmdirSync(path)
     }
 }
 
 const folder = process.argv.slice(2)[0]
 
 if (folder) {
-    deleteFolderRecursive(join(__dirname, "../build", folder))
+    deleteFolderRecursive(Path.join(__dirname, "../build", folder))
 } else {
-    deleteFolderRecursive(join(__dirname, "../build/cjs"))
-    deleteFolderRecursive(join(__dirname, "../build/esm"))
-    deleteFolderRecursive(join(__dirname, "../build/umd"))
-    deleteFolderRecursive(join(__dirname, "../build/types"))
+    deleteFolderRecursive(Path.join(__dirname, "../build/cjs"))
+    deleteFolderRecursive(Path.join(__dirname, "../build/esm"))
+    deleteFolderRecursive(Path.join(__dirname, "../build/umd"))
+    deleteFolderRecursive(Path.join(__dirname, "../build/types"))
 }
